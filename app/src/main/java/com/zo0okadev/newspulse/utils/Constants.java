e9a43df8e8d027fe27b1d4f5eda5e3058f1d13d5
Hazem Dahab <enghazemdahab82@gmail.com>
2019-04-10 03:16:08 +0200

    Removed API KEY

--- app/src/main/java/com/zo0okadev/newspulse/utils/Constants.java
+++ app/src/main/java/com/zo0okadev/newspulse/utils/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
             "theobserver", "travel/offers", "voluntary-sector-network", "weather", "women-in-leadership",
             "working-in-development");
 
-    public static final String API_KEY = "103f2591-6a1a-43ab-a349-d7a887c336c3";
+    public static final String API_KEY = "YOUR_API_KEY";
     public static final String NEWS_API_URL = "http://content.guardianapis.com/";
     public static final String NEWS_LIST_FILTERS = "&order-by=newest&show-fields=thumbnail";
     public static final String NEWS_ARTICLE_FILTERS = "&show-fields=headline,body,hasStoryPackage,standfirst,shortUrl,thumbnail,byline,&show-tags=contributor,keyword,&show-elements=all&show-story-package=true&show-editors-picks=true&show-related=true&show-most-viewed=true";
